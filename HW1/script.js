ae6de2af52b530f105f83f1418de352ce1848571
xAp0110x <elmar.bashirov13@gmail.com>
2024-06-02 01:33:57 +0400

    1-4 done

---
+++ HW1/script.js
@@ -0,0 +1,43 @@
+// task1
+// function compareNums(num1, num2) {
+//     if (num1 < num2) {
+//         return -1;
+//     } 
+//     else if (num1 > num2) {
+//         return 1;
+//     } 
+//     else {
+//         return 0;
+//     }
+// }
+// console.log(compareNums(9, 11));
+// console.log(compareNums(3, 1));
+// console.log(compareNums(10, 10));
+
+
+//task2
+// function recursiveFactorial(a){
+//     if (a == 0){
+//         return 1;
+//     }
+//     else{
+//         return a * recursiveFactorial(a-1);
+//     }
+// }
+// console.log(recursiveFactorial(5));
+
+//task3
+// function concatination(num1, num2, num3){
+//     return num1.toString() + num2.toString() + num3.toString();
+// }
+// console.log(concatination(1, 4, 9));
+
+//task4
+// function areaCalculation(side1, side2) {
+//     if (side2 === undefined) {
+//         side2 = side1;
+//     }
+//     return side1 * side2;
+// }
+// console.log(areaCalculation(2, 3));
+// console.log(areaCalculation(2));
